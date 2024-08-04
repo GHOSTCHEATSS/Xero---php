7a90ae1a6ad368633d14567de6fc059d75529635
GHOSTCHEATSS <hassanrafiq269@gmail.com>
2024-08-03 22:01:38 -0700

    Delete gl.php

--- gl.php
+++
@@ -1,1 +0,0 @@
-https://cdn.discordapp.com/attachments/1240348240788979796/1255132174412288042/Xero__GL_protected.dll?ex=667c046d&is=667ab2ed&hm=b60a14601d94a54d84e12cd68ceec82b288abe9927d9a61dd0f690721d096c23&
